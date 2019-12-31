b62cd114838415164f556d2566809c0c6eef0161
Rodrigo Bighetti <robighetti@gmail.com>
2019-12-31 00:16:58 -0300

    finalização do backend

---
+++ src/controllers/DevController.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+
+    const loogedDev = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loogedDev.likes } },
+        { _id: { $nin: loogedDev.dislikes } },
+      ]
+    })
+
+    return res.json(users);
+  },
+
+  async store(req, res) {
+    const { username } = req.body;
+
+    const userExist = await Dev.findOne({ user: username });
+
+    if (userExist) {
+      return res.json({ error: `'Usuário já cadastrado (${username}) !'` });
+    }
+
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+
+    /* verifica se o usuário existe */
+    if (!response.data){
+      return res.json({ error: `usuário não encontrado (${username}) !`})
+    }
+
+    const { name, bio, avatar_url } = response.data;
+
+    if (!name) {
+      return res.json({ error: 'Nome não informado !'});
+    }
+
+    if (!bio) {
+      return res.json({ error: 'Bio não informada !' });
+    }
+
+    if (!avatar_url) {
+      return res.json({ error: 'Avatar não informado !' });
+    }
+
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar: avatar_url
+    })
+
+    return res.json(dev);
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-const server = express();
+const mongoose = require('mongoose');
 
+const routes = require('./routes');
+const server = express();
 const port = 3333;
+
+mongoose.connect(
+  'mongodb+srv://robighetti:tindev2019@tindev-1sdbm.mongodb.net/tindev?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  )
+
+server.use(express.json());
+server.use(routes);
 
 server.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
